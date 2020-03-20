48b46c381196bc6f7e3f9808531ab7b31e312cab
nile <453199769@qq.com>
2020-03-20 21:08:09 +0800

    change j_search

--- j_search.py
+++ j_search.py
@@ -1,16 +1,7 @@
-def search(l, key):
-    low = 0
-    high = len(l) - 1
+import sys
 
-    while low <= high:
-        mid = (low + high) // 2
-        if l[mid] < key:
-            low = mid + 1
-        elif l[mid] > key:
-            high = mid - 1
-        else:
-            return mid
-    return
+# 获取命令行参数
+print(sys.argv)
 
-l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print("key index is:", search(l, 6))  # key index is: 5
+for i in sys.argv:
+    print(i)
